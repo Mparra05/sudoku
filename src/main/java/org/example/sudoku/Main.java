071835ce4aa2785c9fe74917a472543fd68ebe22
Manuel Parra <parra.manuel@correounivalle.edu.co>
2024-04-27 01:23:43 -0500

    Sudoku Game Finished

--- src/main/java/org/example/sudoku/Main.java
+++ src/main/java/org/example/sudoku/Main.java
@@ -6,12 +6,24 @@ import org.example.sudoku.view.GameStage;
 
 import java.io.IOException;
 
+/**
+ * Main class representing the application
+ */
 public class Main extends Application {
 
+    /**
+     * Main method
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Starts the application
+     * @param stage Application stage
+     * @throws IOException if the game stage gets an error
+     */
     @Override
     public void start(Stage stage) throws IOException {
         GameStage.getInstance();
